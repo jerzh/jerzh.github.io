61d6e765b685ddc652f51cc209a48268e39a64d2
jeremyzhou <jeremy.q.zhou@gmail.com>
2022-09-03 01:22:07 -0400

    Modularize code :thinking:

--- src/domain-color.tsx
+++ src/domain-color.tsx
@@ -49,7 +49,38 @@ function GraphCanvas(props: { exp: string, range: number, center: number[], calc
 }
 
 
-function DomainColorInput(props: { exp: { cur: string, display: string }, setExp, graphSettings }) {
+// Is there a way to not duplicate this here and in DomainColor()?
+interface expStateT {
+  cur: string,
+  display: string,
+}
+
+
+enum GraphType {
+  'Domain coloring',
+  'Julia set',
+  'Mandelbrot set',
+}
+
+
+interface graphSettingsT {
+  graphType: GraphType;
+  range: number;
+  cRe: number;
+  cIm: number;
+  centerX: number;
+  centerY: number;
+}
+
+
+// so much boilerplate oh no
+function DomainColorHeader(
+  props: {
+    exp: expStateT,
+    setExp: (exp: expStateT) => void,
+    graphSettings: graphSettingsT,
+  }
+) {
   var exp = props.exp
   var setExp = props.setExp
   var graphSettings = props.graphSettings
@@ -93,20 +124,170 @@ function DomainColorInput(props: { exp: { cur: string, display: string }, setExp
 }
 
 
-// Big component for entire DomainColor mini-project
-// Should definitely split this into multiple components
+function DomainColorInput(
+  props: {
+    graphSettings: graphSettingsT,
+    setGraphSettings: (graphSettings: graphSettingsT) => void,
+  }
+) {
+  var graphSettings = props.graphSettings
+  var setGraphSettings = props.setGraphSettings
+  return (
+    <div className='center vertical spaced description'>
+      <div className='center horizontal'>
+        <label> range: </label>
+        <input type='number' min='0.05' max='10' step='0.05' value={graphSettings.range}
+          onChange={(event: React.FormEvent<HTMLInputElement>) => {
+            setGraphSettings({
+              ...graphSettings,
+              'range': parseFloat(event.currentTarget.value),
+            })
+          }}
+        />
+      </div>
+      <div className='center horizontal'>
+        <label> center: </label>
+        <input type='number' min='-10' max='10' step='0.05' value={graphSettings.centerX}
+          onChange={(event: React.FormEvent<HTMLInputElement>) => {
+            setGraphSettings({
+              ...graphSettings,
+              'centerX': parseFloat(event.currentTarget.value),
+            })
+          }}
+        />
+        <input type='number' min='-10' max='10' step='0.05' value={graphSettings.centerY}
+          onChange={(event: React.FormEvent<HTMLInputElement>) => {
+            setGraphSettings({
+              ...graphSettings,
+              'centerY': parseFloat(event.currentTarget.value),
+            })
+          }}
+        />
+      </div>
+      <div className='center2 vertical'>
+        {graphSettings.graphType === GraphType['Mandelbrot set'] &&
+          <label> z = {math.complex(graphSettings.cRe, graphSettings.cIm).toString()} </label>
+        ||
+          <label> c = {math.complex(graphSettings.cRe, graphSettings.cIm).toString()} </label>
+        }
+        <div className='center horizontal'>
+          <input type='number' min='-10' max='10' step='0.05' value={graphSettings.cRe}
+            onChange={(event: React.FormEvent<HTMLInputElement>) => {
+              setGraphSettings({
+                ...graphSettings,
+                'cRe': parseFloat(event.currentTarget.value),
+              })
+            }}
+          />
+          <input type='number' min='-10' max='10' step='0.05' value={graphSettings.cIm}
+            onChange={(event: React.FormEvent<HTMLInputElement>) => {
+              setGraphSettings({
+                ...graphSettings,
+                'cIm': parseFloat(event.currentTarget.value),
+              })
+            }}
+          />
+        </div>
+      </div>
+      <div className='center horizontal'>
+        <div className='center vertical'>
+          { // Ugly workaround because of the way TS implements enums
+          Object.keys(GraphType)
+            .filter(k => isNaN(Number(k)))
+            .map(typeName => (
+            <div key={typeName}>
+              <input type='radio' name='graph-type' value={typeName}
+                checked={typeName === GraphType[graphSettings.graphType]}
+                onChange={() => {
+                  setGraphSettings({
+                    ...graphSettings,
+                    'graphType': GraphType[typeName],
+                    'cRe': 0,
+                    'cIm': 0,
+                  })
+                }} />
+              <label> {typeName} </label>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+function GraphCanvasWrapper(
+  props: {
+    exp: expStateT,
+    graphSettings: graphSettingsT,
+  }
+) {
+  var exp = props.exp
+  var graphSettings = props.graphSettings
+  const maxIter = 20
+  return (
+    <div className='center2 vertical description fixed-height'>
+      {/* Draw GraphCanvas with the relevant calcResult function. (There is most definitely a better way)
+      Lol rip the vim syntax coloring is confused */}
+      {exp && (graphSettings.graphType === GraphType['Domain coloring'] &&
+        <GraphCanvas exp={exp.display} range={graphSettings.range}
+          center={[graphSettings.centerX, graphSettings.centerY]}
+          calcResult={(x: number, y: number, f: math.EvalFunction) => {
+            const res = math.complex(f.evaluate({
+              z: math.complex(x, y),
+              c: math.complex(graphSettings.cRe, graphSettings.cIm)
+            })).toPolar()
+            return hsv2rgb(res.phi / (2 * Math.PI), sigmoid(res.r), 1)
+          }}
+        />
+      || graphSettings.graphType == GraphType['Julia set'] &&
+        <GraphCanvas exp={exp.display} range={graphSettings.range}
+          center={[graphSettings.centerX, graphSettings.centerY]}
+          calcResult={(x: number, y: number, f: math.EvalFunction) => {
+            let z = math.complex(x, y)
+            const c = math.complex(graphSettings.cRe, graphSettings.cIm)
+            let count = 0
+            while (z.toPolar().r < 2 && count < maxIter) {
+              count += 1
+              z = math.complex(f.evaluate({
+                z: z,
+                c: c
+              }))
+            }
+            return count !== maxIter ? hsv2rgb(count / maxIter, 1, 1) : [0, 0, 0]
+          }}
+        />
+      || graphSettings.graphType === GraphType['Mandelbrot set'] &&
+        <GraphCanvas exp={exp.display} range={graphSettings.range}
+          center={[graphSettings.centerX, graphSettings.centerY]}
+          calcResult={(x: number, y: number, f: math.EvalFunction) => {
+            let z = math.complex(graphSettings.cRe, graphSettings.cIm)
+            const c = math.complex(x, y)
+            let count = 0
+            while (z.toPolar().r < 2 && count < maxIter) {
+              count += 1
+              z = math.complex(f.evaluate({
+                z: z,
+                c: c
+              }))
+            }
+            return count !== maxIter ? hsv2rgb(count / maxIter, 1, 1) : [0, 0, 0]
+          }}
+        />
+      )}
+    </div>
+  )
+}
+
+
 function DomainColor(props: {}) {
-  // Lol this is kinda messy, maybe move to Fresh or something
-  // Actually how about I put everything in an object so I'm not passing
-  // around a bajillion variables and I can actually split up the components
   const [exp, setExp] = React.useState({
     'cur': 'z^2 + c',
     'display': 'z^2 + c',
   })
 
   const [graphSettings, setGraphSettings] = React.useState({
-    // Does JS have a builtin enum type?
-    'graphTypeIndex': 0,
+    'graphType': GraphType['Domain coloring'],
     'range': 3,
     'cRe': 0,
     'cIm': 0,
@@ -114,140 +295,12 @@ function DomainColor(props: {}) {
     'centerY': 0,
   })
 
-  const graphTypeNames = ['Domain coloring', 'Julia set', 'Mandelbrot set']
-  const maxIter = 20
-
   return (
     <>
-      <DomainColorInput exp={exp} setExp={setExp} graphSettings={graphSettings} />
+      <DomainColorHeader exp={exp} setExp={setExp} graphSettings={graphSettings} />
       <div className='section center horizontal'>
-        <div className='center2 vertical description fixed-height'>
-          {/* Draw GraphCanvas with the relevant calcResult function. (There is most definitely a better way)
-          Lol rip the vim syntax coloring is confused */}
-          {exp && (graphTypeNames[graphSettings.graphTypeIndex] === 'Domain coloring' &&
-            <GraphCanvas exp={exp.display} range={graphSettings.range}
-              center={[graphSettings.centerX, graphSettings.centerY]}
-              calcResult={(x: number, y: number, f) => {
-                const res = math.complex(f.evaluate({
-                  z: math.complex(x, y),
-                  c: math.complex(graphSettings.cRe, graphSettings.cIm)
-                })).toPolar()
-                return hsv2rgb(res.phi / (2 * Math.PI), sigmoid(res.r), 1)
-              }}
-            />
-          || graphTypeNames[graphSettings.graphTypeIndex] == 'Julia set' &&
-            <GraphCanvas exp={exp.display} range={graphSettings.range}
-              center={[graphSettings.centerX, graphSettings.centerY]}
-              calcResult={(x: number, y: number, f) => {
-                let z = math.complex(x, y)
-                const c = math.complex(graphSettings.cRe, graphSettings.cIm)
-                let count = 0
-                while (z.toPolar().r < 2 && count < maxIter) {
-                  count += 1
-                  z = math.complex(f.evaluate({
-                    z: z,
-                    c: c
-                  }))
-                }
-                return count !== maxIter ? hsv2rgb(count / maxIter, 1, 1) : [0, 0, 0]
-              }}
-            />
-          || graphTypeNames[graphSettings.graphTypeIndex] === 'Mandelbrot set' &&
-            <GraphCanvas exp={exp.display} range={graphSettings.range}
-              center={[graphSettings.centerX, graphSettings.centerY]}
-              calcResult={(x: number, y: number, f) => {
-                let z = math.complex(graphSettings.cRe, graphSettings.cIm)
-                const c = math.complex(x, y)
-                let count = 0
-                while (z.toPolar().r < 2 && count < maxIter) {
-                  count += 1
-                  z = math.complex(f.evaluate({
-                    z: z,
-                    c: c
-                  }))
-                }
-                return count !== maxIter ? hsv2rgb(count / maxIter, 1, 1) : [0, 0, 0]
-              }}
-            />
-          )}
-        </div>
-        {/* All the inputs */}
-        <div className='center vertical spaced description'>
-          <div className='center horizontal'>
-            <label> range: </label>
-            <input type='number' min='0.05' max='10' step='0.05' value={graphSettings.range}
-              onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                setGraphSettings({
-                  ...graphSettings,
-                  'range': parseFloat(event.currentTarget.value),
-                })
-              }}
-            />
-          </div>
-          <div className='center horizontal'>
-            <label> center: </label>
-            <input type='number' min='-10' max='10' step='0.05' value={graphSettings.centerX}
-              onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                setGraphSettings({
-                  ...graphSettings,
-                  'centerX': parseFloat(event.currentTarget.value),
-                })
-              }}
-            />
-            <input type='number' min='-10' max='10' step='0.05' value={graphSettings.centerY}
-              onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                setGraphSettings({
-                  ...graphSettings,
-                  'centerY': parseFloat(event.currentTarget.value),
-                })
-              }}
-            />
-          </div>
-          <div className='center2 vertical'>
-            {graphTypeNames[graphSettings.graphTypeIndex] === 'Mandelbrot set' &&
-              <label> z = {math.complex(graphSettings.cRe, graphSettings.cIm).toString()} </label>
-            ||
-              <label> c = {math.complex(graphSettings.cRe, graphSettings.cIm).toString()} </label>
-            }
-            <div className='center horizontal'>
-              <input type='number' min='-10' max='10' step='0.05' value={graphSettings.cRe}
-                onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                  setGraphSettings({
-                    ...graphSettings,
-                    'cRe': parseFloat(event.currentTarget.value),
-                  })
-                }}
-              />
-              <input type='number' min='-10' max='10' step='0.05' value={graphSettings.cIm}
-                onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                  setGraphSettings({
-                    ...graphSettings,
-                    'cIm': parseFloat(event.currentTarget.value),
-                  })
-                }}
-              />
-            </div>
-          </div>
-          <div className='center horizontal'>
-            <div className='center vertical'>
-              {graphTypeNames.map((typeName, typeIndex) => (
-                <div key={typeName}>
-                  <input type='radio' name='graph-type' value={typeName}
-                    checked={typeIndex === graphSettings.graphTypeIndex}
-                    onChange={() => {
-                      setGraphSettings({
-                        ...graphSettings,
-                        'graphTypeIndex': typeIndex,
-                        'cRe': 0,
-                        'cIm': 0,
-                      })
-                    }} />
-                  <label> {typeName} </label>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
+        <GraphCanvasWrapper exp={exp} graphSettings={graphSettings} />
+        <DomainColorInput graphSettings={graphSettings} setGraphSettings={setGraphSettings} />
       </div>
     </>
   )
